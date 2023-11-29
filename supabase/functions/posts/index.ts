025929a0772ed6bdc8589bcc9e6e7b76abb7d35b
natanaelvich <taelima1997@gmail.com>
2023-11-28 22:27:09 -0300

    Add Supabase serverless function for fetching posts

---
+++ supabase/functions/posts/index.ts
@@ -0,0 +1,28 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
+
+Deno.serve(async (req) => {
+  try {
+    const supabase = createClient(
+      Deno.env.get("SUPABASE_URL") ?? "",
+      Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+      {
+        global: {
+          headers: { Authorization: req.headers.get("Authorization")! },
+        },
+      }
+    );
+
+    const { data, error } = await supabase.from("posts").select("*");
+
+    if (error) {
+      throw error;
+    }
+
+    return new Response(JSON.stringify({ data }), {
+      headers: { "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (err) {
+    return new Response(String(err?.message ?? err), { status: 500 });
+  }
+});
