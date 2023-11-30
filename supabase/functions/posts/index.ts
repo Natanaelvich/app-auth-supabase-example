50a239efef4a00d8b2c07bc41035b70f73dfaf5e
natanaelvich <taelima1997@gmail.com>
2023-11-29 23:10:17 -0300

    Update Supabase environment variables

--- supabase/functions/posts/index.ts
+++ supabase/functions/posts/index.ts
@@ -3,8 +3,8 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js";
 Deno.serve(async (req) => {
   try {
     const supabase = createClient(
-      Deno.env.get("SUPABASE_URL") ?? "",
-      Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+      Deno.env.get("SUPA_URL") ?? "",
+      Deno.env.get("SUPA_ANON_KEY") ?? "",
       {
         global: {
           headers: { Authorization: req.headers.get("Authorization")! },
