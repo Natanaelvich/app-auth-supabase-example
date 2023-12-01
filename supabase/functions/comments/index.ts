afd6860b1fab206216d1c973240e11b238278d20
natanaelvich <taelima1997@gmail.com>
2023-11-30 21:46:51 -0300

    Add Supabase comments function

---
+++ supabase/functions/comments/index.ts
@@ -0,0 +1,24 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+Deno.serve(async (req: Request) => {
+  try {
+    console.log("req", req)
+    const supabase = createClient(
+      Deno.env.get("SUPA_URL") ?? "",
+      Deno.env.get("SUPA_ANON_KEY") ?? ""
+    );
+
+    const { data, error } = await supabase.from("comments").select("*");
+
+    if (error) {
+      throw error;
+    }
+
+    return new Response(JSON.stringify({ data }), {
+      headers: { "Content-Type": "application/json" },
+      status: 200,
+    });
+  } catch (err) {
+    return new Response(String(err?.message ?? err), { status: 500 });
+  }
+});
